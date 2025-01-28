7ce7d971737e68e5121ed3b504cf9af9a686e999
Luisa <luisaferreirass08@gmail.com>
2025-01-28 15:34:23 -0300

    improve: adding validator in registry_order

--- src/use_cases/registry_order.py
+++ src/use_cases/registry_order.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from src.models.repository.interfaces.orders_repository import OrdersRepositoryInterface
 from src.main.http_types.http_request import HttpRequest
 from src.main.http_types.http_response import HttpResponse
+from src.validators.registry_order_validator import registry_order_validator
 
 class RegistryOrder:
     def __init__(self, orders_repository: OrdersRepositoryInterface):
@@ -10,6 +11,8 @@ class RegistryOrder:
     def registry(self, http_request: HttpRequest) -> HttpResponse:
         try:
             body = http_request.body
+            self.__validate_body(body)
+
             new_order = self.__format_new_order(body)
 
             self.__registry_order(new_order)
@@ -17,10 +20,12 @@ class RegistryOrder:
             return self.__format_response()
         except Exception as exception:
             return HttpResponse(
-                body= { "error": exception },
+                body= { "error": str(exception) },
                 status_code=400
             )
 
+    def __validate_body(self, body: dict) -> None:
+        registry_order_validator(body)
 
     def __format_new_order(self, body: dict) -> dict:
         new_order = body["data"]
