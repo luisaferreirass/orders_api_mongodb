514eb18942159557b73c30f0125ef3f2499fb058
Luisa <luisaferreirass08@gmail.com>
2025-01-28 17:43:24 -0300

    feat: implementing registry_finder in use_cases

--- src/main/http_types/http_request.py
+++ src/main/http_types/http_request.py
@@ -1,5 +1,11 @@
 class HttpRequest:
-    def __init__(self, body: dict = None, headers: dict = None):
+    def __init__(self, 
+                body: dict = None,
+                headers: dict = None,
+                path_params: dict = None,
+                query: dict = None):
         self.body = body
         self.headers = headers
+        self.path_params = path_params
+        self.query = query
         
